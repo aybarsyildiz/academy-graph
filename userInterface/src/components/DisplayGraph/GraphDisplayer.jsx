7bb2a625f7a79c474b1a8867f98ddccf338869be
Tayipztrk <tayipztrk@icloud.com>
2022-05-15 14:08:54 +0300

    graphEditer edit

--- userInterface/src/components/DisplayGraph/GraphDisplayer.jsx
+++ userInterface/src/components/DisplayGraph/GraphDisplayer.jsx
@@ -1,58 +1,54 @@
 import NeoVis from "neovis.js";
 //https://github.com/neo4j-contrib/neovis.js/
 import { useEffect, useState } from "react";
-import { Link  } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { NeoGraph } from "./NeoGraph";
 
-// function DrawViz(){
-//     var config={
-//         containerId: "viz",
-//         neo4j: {
-//             serverUrl: "neo4j+s://18f4b03e.databases.neo4j.io",
-//             serverUser: "neo4j",
-//             serverPassword: "u1_rMzco54I1jH-Eiz7bEhWV32fiJLhhnmkOzBnHgv4"
-//         },
-//         labels: {
-//             Person:{
-//                 label:"name"
-//             },
-//             Publish:{
-//                 label:"name"
-//             },
-//         },
-//         relationships:{
-//             INTERACTS:{
-//                 value: "Publish"
-//             }
-//         },
-//         initialCypher: `match (n) return n limit 25`,
+const NEO4J_USER = "neo4j";
+const NEO4J_PASSWORD = "u1_rMzco54I1jH-Eiz7bEhWV32fiJLhhnmkOzBnHgv4";
+const NEO4J_URI = "neo4j+s://18f4b03e.databases.neo4j.io";
 
-//     };
-//     //get configuration and use them on Neovis
-//     var viz = new Neovis(config);
-//     console.log(viz);
+// function DrawViz() {
+//   var config = {
+//     containerId: "viz",
+//     neo4j: {
+//       serverUrl: "neo4j+s://18f4b03e.databases.neo4j.io",
+//       serverUser: "neo4j",
+//       serverPassword: "u1_rMzco54I1jH-Eiz7bEhWV32fiJLhhnmkOzBnHgv4",
+//     },
+//     labels: {
+//       Person: {
+//         label: "name",
+//       },
+//       Publish: {
+//         label: "name",
+//       },
+//     },
+//     relationships: {
+//       INTERACTS: {
+//         value: "Publish",
+//       },
+//     },
+//     initialCypher: `match (n) return n limit 25`,
+//   };
+//   //get configuration and use them on Neovis
+//   var viz = new Neovis(config);
+//   console.log(viz);
 
-//     //if there is context in viz render viz else render something else
-//     if(viz != null){
-//         return viz.render();
-//     }
-//     else{
-//         return(
-//             <div>
-//                 <h1>Veriler Getirilemedi</h1>
-//             </div>
-//         )
-//     }
-
+//   //if there is context in viz render viz else render something else
+//   if (viz != null) {
+//     return viz.render();
+//   } else {
+//     return (
+//       <div>
+//         <h1>Veriler Getirilemedi</h1>
+//       </div>
+//     );
+//   }
 // }
 
 export default function DisplayGraph() {
   const [data, setData] = useState([]);
-
-//   const history = useHistory ();
-//   const routeChange = () => {
-    
-//     history('/login');
-//   };
 
   const fetchData = async () => {
     const response = await fetch("http://localhost:5000/api/v1/getNodes");
@@ -60,9 +56,11 @@ export default function DisplayGraph() {
     setData(json);
     console.log(json);
   };
+
   useEffect(() => {
     fetchData();
   }, []);
+
   return (
     <div className="graphDisplayer">
       <div className="graphDisplayerWrapper">
@@ -75,11 +73,22 @@ export default function DisplayGraph() {
           }}
         >
           <h3 className="graphDisplayerLogo">Academian Graph</h3>
+
           <Link to="/login" className="loginButton">
             admin girişi
           </Link>
         </div>
-        <div className="graphDisplayerReal"></div>
+        <div className="graphDisplayerReal">
+          <NeoGraph
+            width={400}
+            height={300}
+            containerId={"id1"}
+            neo4jUri={NEO4J_URI}
+            neo4jUser={NEO4J_USER}
+            neo4jPassword={NEO4J_PASSWORD}
+            backgroundColor={"#b2beb5"}
+          />
+        </div>
       </div>
     </div>
   );
--- userInterface/src/components/login/Login.jsx
+++ userInterface/src/components/login/Login.jsx
@@ -2,49 +2,63 @@ import { useContext, useRef } from "react";
 import "./login.css";
 import { loginCall } from "../../apiCalls";
 import { AuthContext } from "../../context/AuthContext";
-import { CircularProgress } from "@material-ui/core";   
+import { CircularProgress } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-    const Username = useRef();
-    const Password = useRef();
-    const { isFetching, dispatch, error } = useContext(AuthContext);
+  const Username = useRef();
+  const Password = useRef();
+  const { isFetching, dispatch, error } = useContext(AuthContext);
 
-
-    const handleClick = (e) => {
-        e.preventDefault();
-        loginCall(
-            { Username: Username.current.value, Password: Password.current.value },
-            dispatch
-          );
-    };
-
-    return(
-        <div className="login">
-            <div className="loginWrapper">
-                <div className="loginLeft">
-                    <h3 className="loginLogo">Akademisyen Makale Graph</h3>
-                </div>
-                <div className="loginRight">
-                    <form className="loginBox" onSubmit={handleClick}>
-                            {error === true ? (
-                                <div className="error-message">Login Failure</div>
-                            ) : (
-                                "Admin Girişi"
-                            )}
-                        <input placeholder="Kullanıcı Adı" className="loginInput" type="Username" required ref={Username} />
-                        <input placeholder="Şifre" className="loginInput" type="Password" required ref={Password} />
-                        <button className="loginButton" type="submit" >
-                            Giriş Yap
-                        </button>
-                        
-                        <span className="loginForgot">Şifreni mi unuttun?</span>
-                    </form>
-                    <button className="loginButton" >
-                            <a href="/displayGraph"></a>
-                            Graph Görüntüle
-                    </button>
-                </div>
-            </div>
-        </div>
+  const handleClick = (e) => {
+    e.preventDefault();
+    loginCall(
+      { Username: Username.current.value, Password: Password.current.value },
+      dispatch
     );
+  };
+
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Akademisyen Makale Graph</h3>
+        </div>
+        <div className="loginRight">
+          <form className="loginBox" onSubmit={handleClick}>
+            {error === true ? (
+              <div className="error-message">Login Failure</div>
+            ) : (
+              "Admin Girişi"
+            )}
+            <input
+              placeholder="Kullanıcı Adı"
+              className="loginInput"
+              type="Username"
+              required
+              ref={Username}
+            />
+            <input
+              placeholder="Şifre"
+              className="loginInput"
+              type="Password"
+              required
+              ref={Password}
+            />
+            <button className="loginButton" type="submit">
+              Giriş Yap
+            </button>
+
+            <span className="loginForgot">Şifreni mi unuttun?</span>
+          </form>
+          <button className="loginButton">
+          <Link to="/displayGraph" style={{color: 'white'}}>
+          Graph Görüntüle
+          </Link>
+          </button>
+          
+        </div>
+      </div>
+    </div>
+  );
 }
